65f649a8c5e956ae1b92756aa8898fd9bf307097
Paunczki <johnnymikos@gmail.com>
2020-01-15 17:15:47 -0600

    Work on homework

---
+++ Homework/Homework 1/work.py
@@ -0,0 +1,7 @@
+'''
+I plan to do a simple classifier that only judges good or bad based on how many characters in a sentence
+I will count the avearge number of characters and do a binary choice
+'''
+
+# need to do while loops to loop through
+
