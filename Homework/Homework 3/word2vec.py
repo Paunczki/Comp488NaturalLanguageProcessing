79ac2e94ceeffd2cbecfeb3c40752129e6c22901
Paunczki <johnnymikos@gmail.com>
2020-03-11 16:39:55 -0500

    Some setup for homework 3

--- Homework/Homework 3/word2vec.py
+++ Homework/Homework 3/word2vec.py
@@ -1,1 +1,26 @@
-# implements word2vec
+# implements word2vec from gensim
+# https://radimrehurek.com/gensim/auto_examples/tutorials/run_word2vec.html
+
+from gensim.test.utils import datapath
+from gensim import utils
+
+class MyCorpus(object):
+    # print("hello")
+    def __iter__(self):
+        corpus_path = datapath('lee_background.cor')
+        for line in open(corpus_path):
+            yield utils.simple_preprocess(line)
+
+
+import gensim.models
+
+sentences = MyCorpus()
+model = gensim.models.Word2Vec(sentences=sentences)
+
+vec_king = model.wv['king']
+
+for i,word in enumerate(model.wv.vocab):
+    if i == 10:
+        break
+    print(word)
+
